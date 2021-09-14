0111309e86eb62ac7677d23fb512f3eef53ce5ce
Traian Anghel <tanghel@live.com>
2021-09-14 21:18:41 +0300

    removed type from account nfts

--- src/endpoints/tokens/token.service.ts
+++ src/endpoints/tokens/token.service.ts
@@ -479,10 +479,10 @@ export class TokenService {
       //   }
       // }
 
-      let gatewayNftDetails = await this.getNft(nft.collection);
-      if (gatewayNftDetails) {
-        nft.type = gatewayNftDetails.type;
-      }
+      // let gatewayNftDetails = await this.getNft(nft.collection);
+      // if (gatewayNftDetails) {
+      //   nft.type = gatewayNftDetails.type;
+      // }
 
       nfts.push(nft);
     }
